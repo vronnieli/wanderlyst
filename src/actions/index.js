955e864053c56b001f03b1bbf69c200810d68136
vronnieli <vronnieli@gmail.com>
2016-10-04 16:37:17 -0400

    removed debugger

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,6 @@
 const BASE_URL = 'https://wanderlyst-api.herokuapp.com/'
 
 export function searchedItineraries(searchTerm){
-  debugger;
   const itineraries = fetch(`${BASE_URL}api/v1/itineraries`).then((response) => {return response.json()}).then((itinerariesPayload) => {return itinerariesPayload})
   return {
     type: 'SEARCHED_ITINERARIES',
